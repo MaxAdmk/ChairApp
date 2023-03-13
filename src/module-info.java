d478e867e2042c09bffaf7a92e2fd353c44f68d7
MaxAdmk <maksimkeny@gmail.com>
2023-03-13 15:04:38 +0200

    Fixed problems in code

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author maksi
+ *
+ */
+module ua.lviv.iot.algo.part1.chairApp {
+}
--- src/ua/lviv/iot/algo/part1/chairApp/Chair.java
+++ src/ua/lviv/iot/algo/part1/chairApp/Chair.java
@@ -14,7 +14,11 @@ public class Chair {
     private static Chair instance = new Chair();
 
     public void occupy(String owner) {
-        this.owner = owner;
+        if(this.owner == null) {
+            this.owner = owner;
+        } else {
+            System.out.println("This chair is occupied");
+        }
     }
 
     public void release() {
@@ -22,11 +26,7 @@ public class Chair {
     }
 
     public boolean isOccupied() {
-        if (this.owner != null) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.owner != null;
     }
 
     public static Chair getInstance() {
@@ -40,23 +40,14 @@ public class Chair {
     }
 
     public static void main(String[] args) {
-        Chair officeChair = new Chair("Oak", 100, 48, "Pedro");
-        officeChair.occupy("Vasyl");
-        System.out.println(officeChair.isOccupied());
-        officeChair.release();
-        System.out.println(officeChair.isOccupied());
 
-        Chair[] chairs = new Chair[4];
-        chairs[0] = Chair.getInstance();
-        chairs[1] = new Chair();
-        chairs[2] = new Chair("Birch", 95, 30, "Maksym");
-        chairs[3] = Chair.getInstance();
+        Chair[] chairs = {
+        Chair.getInstance(),
+        new Chair(),
+        new Chair("Birch", 95, 30, "Maksym"),
+        Chair.getInstance()};
 
-        for (int i=0; i < chairs.length; i++) {
-            System.out.println("Chair id:" + chairs[i].getId());
-        }
-
-        System.out.println(officeChair.toString());
+        for (Chair info: chairs) System.out.println(info);
 
     }
 }
